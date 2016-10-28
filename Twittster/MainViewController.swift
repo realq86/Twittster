9f9f399dea2f07334fc8d3de8c5bc5bd6218ad10
Michael <realq86@gmail.com>
2016-10-27 23:13:56 -0700

    Create new Main View Controller

---
+++ Twittster/MainViewController.swift
@@ -0,0 +1,61 @@
+//
+//  MainViewController.swift
+//  Twittster
+//
+//  Created by Developer on 10/27/16.
+//  Copyright © 2016 Developer. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tableViewDataBackArray = [AnyObject]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setupTableView()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func setupTableView() {
+        self.automaticallyAdjustsScrollViewInsets = false
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.tableView.estimatedRowHeight = 100
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return tableViewDataBackArray.count
+        return 5
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "TEST"
+        
+        return cell
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
