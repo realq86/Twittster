abb5afce878011766f1ebcb582d54ea2ef55edb1
Michael <realq86@gmail.com>
2016-10-27 22:16:49 -0700

    Rename ViewController to LoginViewController

---
+++ Twittster/LoginViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Twittster
+//
+//  Created by Developer on 10/27/16.
+//  Copyright © 2016 Developer. All rights reserved.
+//
+
+import UIKit
+import BDBOAuth1Manager
+
+class LoginViewController: UIViewController {
+
+    var twitterClient = TwitterServer.sharedInstance
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    
+
+    
+
+    
+    @IBAction func onLoginButton(_ sender: AnyObject) {
+        
+        
+        self.setupTwitterClient()
+        
+    }
+    
+    func setupTwitterClient() {
+        
+        self.twitterClient.login(success: {
+            
+            
+            
+            },failure: {
+        })
+        
+    }
+    
+
+}
+
